8d9bf0d491d119af046c307e7ae577b9237f9da4
Gopal Khatri <35285671+khatrigopal@users.noreply.github.com>
2025-07-13 13:44:47 +0530

    Update churn_pipeline.py

--- churn_pipeline.py
+++ churn_pipeline.py
@@ -21,7 +21,7 @@ logger = logging.getLogger(__name__)
 region = boto3.Session().region_name
 session = PipelineSession()
 role = "arn:aws:iam::911167906047:role/SageMakerChurnRole"
-bucket = "mlops-churn-model-artifacts"
+bucket = "kg-mlops-churn-model-artifacts"
 
 # Verify S3 bucket exists
 s3 = boto3.client('s3')
